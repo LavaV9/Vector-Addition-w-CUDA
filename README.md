57abc79a887f68c5a2e2f685a60de6ae50759ea8
LavaV9 <vshenoy3@charlotte.edu>
2025-09-18 13:35:50 -0400

    Initial Commit
    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Vector Addition w CUDA
 Simple Project For Learning CUDA Basics (and getting back into C++)
+just adding a commit so it shows up on github
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    int a = 5;
+    int b = 4;
+    int c = a+b;
+    std::cout << c;
+    return 0;
+}
+
+
+// just to test if my compiler set up works till i get hpc access
